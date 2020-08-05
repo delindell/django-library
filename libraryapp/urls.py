7a79f651d80f9f926fee77ee813cd1c079c86b5c
Davis Lindell <daviselindell@gmail.com>
2020-08-05 15:31:26 -0500

    added delete to books

---
+++ libraryapp/urls.py
@@ -0,0 +1,18 @@
+from django.urls import include, path
+from .views import *
+
+app_name = "libraryapp"
+
+urlpatterns = [
+    path('', book_list, name='home'),
+    path('accounts/', include('django.contrib.auth.urls')),
+    path('books/', book_list, name='books'),
+    path('book/form', book_form, name='book_form'),
+    path('books/<int:book_id>/', book_details, name='books'),
+    path('librarians/', list_librarians, name='librarians'),
+    path('librarians/<int:librarian_id>/', librarian_details, name='librarians'),
+    path('libraries/', list_libraries, name='libraries'),
+    path('library/form', library_form, name='library_form'),
+    path('libraries/<int:library_id>/', library_details, name='libraries'),
+    path('logout/', logout_user, name='logout'),
+]
