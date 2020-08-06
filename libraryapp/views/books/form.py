f245c8a392e3da9a051426dcae3e2bb51002ab1e
Davis Lindell <daviselindell@gmail.com>
2020-08-06 11:42:11 -0500

    can edit dem books

--- libraryapp/views/books/form.py
+++ libraryapp/views/books/form.py
@@ -4,6 +4,7 @@ from django.contrib.auth.decorators import login_required
 from libraryapp.models import Book
 from libraryapp.models import Library
 from ..connection import Connection
+from ..books.details import get_book
 
 
 def get_libraries():
@@ -27,6 +28,21 @@ def book_form(request):
         libraries = get_libraries()
         template = 'books/form.html'
         context = {
+            'all_libraries': libraries
+        }
+
+        return render(request, template, context)
+
+@login_required
+def book_edit_form(request, book_id):
+
+    if request.method == 'GET':
+        book = get_book(book_id)
+        libraries = get_libraries()
+
+        template = 'books/form.html'
+        context = {
+            'book': book,
             'all_libraries': libraries
         }
 
