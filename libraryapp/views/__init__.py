25506376392951fa56907716caadf9d05cbdbf8c
Davis Lindell <daviselindell@gmail.com>
2020-08-07 12:28:57 -0500

    refactored data to be more complex and added registration form with navbar links

--- libraryapp/views/__init__.py
+++ libraryapp/views/__init__.py
@@ -7,3 +7,4 @@ from .books.form import book_form, book_edit_form
 from .books.details import book_details
 from .libraries.form import library_form
 from .libraries.details import library_details
+from .auth.register import register_user
